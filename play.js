a685959503e8694064e08082109266e528471b14
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-19 18:27:55 -0500

    add event handler to the client that displays server's message

--- play.js
+++ play.js
@@ -1,8 +1,22 @@
 const net = require("net");
 
-const conn = net.createConnection({
-  host: "192.168.86.109",
-  port: 50541
-});
+const connect = function() {
+  const conn = net.createConnection({
+    host: "192.168.86.109",
+    port: 50541
+  });
+  
+  conn.setEncoding("utf8");
 
-conn.setEncoding("utf8");
+  conn.on("connect", () => console.log("Connected!"));
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+
+connect();
