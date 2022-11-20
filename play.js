efd491acba237bf5710fa9f7a81ab1b369442294
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-19 19:27:53 -0500

    add stdin to play

--- play.js
+++ play.js
@@ -2,4 +2,13 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 
-connect();
+const conn = connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  return stdin;
+};
+
