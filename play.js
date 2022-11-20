4f57c4767184d073a8cdbe361d21f220bdd4397e
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-19 19:38:50 -0500

    add handleUserInput and setupInput functions, implement ctrl c

--- play.js
+++ play.js
@@ -4,11 +4,23 @@ console.log("Connecting ...");
 
 const conn = connect();
 
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    // console.log("Control c pressed by user!");
+    process.exit();
+  }
+};
+
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf-8');
   stdin.resume();
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
   return stdin;
 };
 
+setupInput();
